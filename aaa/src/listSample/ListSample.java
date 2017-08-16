7e5cfd2aecb5f747431b340a6b4f3c18f623b68a
yuki <yuki@Yuki_Tab>
2017-08-16 19:05:12 +0900

    made methods (lambda2_3 and lambda2_4) and added java doc comments

--- aaa/src/listSample/ListSample.java
+++ aaa/src/listSample/ListSample.java
@@ -13,26 +13,64 @@ public class ListSample {
 
 		List<OwnBean> list2 = Arrays.asList(new OwnBean("a"), new OwnBean("b"), new OwnBean("c"), new OwnBean("d"));
 		ls.lambda2(list2);
+		ls.lambda2_2(list2);
+		ls.lambda2_3(list2);
 	}
 
 	public void methodReference(List<String> list) {
+		System.out.println("---------- start methodReference method ----------");
 		list.forEach(System.out::print);
 
 	}
 
 	public void lambda1 (List<String> list) {
+		System.out.println("---------- start lambda1 method ----------");
 		list.forEach((String word)->System.out.println(word));
 	}
 
 	public void lambda1_2 (List<String> list) {
+		System.out.println("---------- start lambda1_2 method ----------");
 		list.forEach(word -> System.out.println(word));
 
 	}
 
 	public void lambda2 (List<OwnBean> list) {
+		System.out.println("---------- start lambda2 method ----------");
 		list.forEach((OwnBean own)->System.out.println(own.getWord()));
 	}
 
+	/**
+	 * 独自クラスを格納するListを
+	 * lambda式でどんなかんじまで再現できるかチャレンジ!!.
+	 * @param list list 独自クラスを格納したリスト
+	 */
+	public void lambda2_2 (List<OwnBean> list) {
+		System.out.println("---------- start lambda2_2 method ----------");
+		list.forEach(own->System.out.println(own.getWord()));
+	}
+
+	/**
+	 * 独自クラスを格納するListを
+	 * lambda式でどんなかんじで再現できるかチャレンジ!!.
+	 * @param list 独自クラスを格納したリスト
+	 */
+	public void lambda2_3 (List<OwnBean> list) {
+		System.out.println("---------- start lambda2_3 method ----------");
+		list.forEach((OwnBean own)->{
+			String tmp = own.getWord();
+			System.out.println(tmp);
+		});
+	}
+
+// TODO why canNOT i create return...?
+//	public void lambda2_4 (List<OwnBean> list) {
+//		System.out.println("---------- start lambda2 method ----------");
+//		String word = list.forEach((OwnBean own)->{
+//			String tmp = own.getWord();
+//			System.out.println(tmp);
+//			return tmp;
+//		});
+//	}
 
 
 
