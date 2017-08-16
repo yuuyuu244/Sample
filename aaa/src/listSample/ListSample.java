58193242306cbdc9470b98c83c54031836d57328
yuki <yuki@Yuki_Tab>
2017-08-16 18:26:02 +0900

    made kinda lamda method

--- aaa/src/listSample/ListSample.java
+++ aaa/src/listSample/ListSample.java
@@ -5,8 +5,27 @@ import java.util.List;
 
 public class ListSample {
 	public static void main (String[] args) {
+		ListSample ls = new ListSample();
 		List<String> list = Arrays.asList("a", "b", "c", "d");
-		list.forEach(System.out::println);
+//		ls.methodReference(list);
+		ls.lambda(list);
+		ls.lambda2(list);
 	}
+
+	public void methodReference(List<String> list) {
+		list.forEach(System.out::print);
+
+	}
+
+	public void lambda (List<String> list) {
+		list.forEach((String word)->System.out.println(word));
+	}
+
+	public void lambda2 (List<String> list) {
+		list.forEach(word -> System.out.println(word));
+
+	}
+
+
 
 }
