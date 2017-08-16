ce9efe96696ceb875ef3fe48adfc887792e8be04
yuki <yuki@Yuki_Tab>
2017-08-16 18:00:42 +0900

    update

--- aaa/src/listSample/ListSample.java
+++ aaa/src/listSample/ListSample.java
@@ -7,6 +7,7 @@ public class ListSample {
 	public static void main (String[] args) {
 		List<String> list = Arrays.asList("a", "b", "c", "d");
 		list.forEach(System.out::println);
+		System.out.println("end msin method.");
 	}
 
 }
