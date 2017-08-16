0bca124c0a423f4b89f9e39a3d6f9b0bbc7e8a7f
yuki <yuki@Yuki_Tab>
2017-08-16 17:53:41 +0900

    made list

--- aaa/src/listSample/ListSample.java
+++ aaa/src/listSample/ListSample.java
@@ -1,8 +1,12 @@
 package listSample;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ListSample {
 	public static void main (String[] args) {
-
+		List<String> list = Arrays.asList("a", "b", "c", "d");
+		list.forEach(System.out::println);
 	}
 
 }
